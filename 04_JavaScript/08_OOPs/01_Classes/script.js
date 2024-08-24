9ccd6deaf9b19294b169c9aad895936de9bdfabf
Darshan R.M <=>
2024-08-24 12:23:51 +0530

    JS Classes

---
+++ 04_JavaScript/08_OOPs/01_Classes/script.js
@@ -0,0 +1,32 @@
+class Color{
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+    innerRGB() {
+        const { r, g, b } = this;
+        return `${r}, ${g}, ${b}`;
+    }
+    rgb() {
+        return `rgb(${this.innerRGB()})`;
+    }
+    rgba(a = 1.0) {
+        return `rgba(${this.innerRGB()}, ${a})`;
+    }
+    hex() {
+        const { r, g, b } = this;
+        return (
+            '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+        );
+    }
+}
+
+
+const c1 = new Color(255, 67, 89, "tomato");            // object creation
+
+console.log(c1.innerRGB());
+console.log(c1.rgb());
+console.log(c1.rgba());
+console.log(c1.hex());
